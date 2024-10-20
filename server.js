2603e5f956a200eb0722aafeae379c5e2551caed
Paulo Cesar <you@example.com>
2024-10-19 21:45:18 -0300

    Adicionando arquivos

--- server.js
+++ server.js
@@ -42,9 +42,17 @@ app.get('/', (req, res) => {
 });
 
 app.use((req, res, next) => {
-    // Define cabeçalhos CORS para permitir a origem específica
-    res.header('Access-Control-Allow-Origin', 'https://cachacascapitao.netlify.app');
-    res.header('Access-Control-Allow-Origin', 'https://cachacascapitao.com.br');
+    const allowedOrigins = [
+        'https://cachacascapitao.netlify.app',
+        'https://cachacascapitao.com.br',
+        'https://www.cachacascapitao.com.br'
+    ];
+    const origin = req.headers.origin;
+
+    if (allowedOrigins.includes(origin)) {
+        res.header('Access-Control-Allow-Origin', origin);
+    }
+
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
